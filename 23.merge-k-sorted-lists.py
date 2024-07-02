fc1618415f62146d8c495caa53edb53342f503f6
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-07-02 16:34:50 -0500

    hard

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -6,12 +6,28 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+import heapq
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        pqueue = []
+        dummy = ListNode()
+        tail = dummy 
+
+        for head in lists:
+            if head:
+                heapq.heappush(pqueue, (head.val,id(head), head))
+        while pqueue:
+            node = heapq.heappop(pqueue)[2]
+            tail.next = node 
+            if node.next:
+                heapq.heappush(pqueue, (node.next.val, id(node.next),node.next))
+            tail = tail.next 
         
+        return dummy.next
+
 # @lc code=end
 

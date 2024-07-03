2a8d8400ba5488f39ef92137745e5b54424984e1
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-07-03 16:11:28 -0500

    linked list pointers done

---
+++ two_pointers/160.intersection-of-two-linked-lists.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=160 lang=python3
+#
+# [160] Intersection of Two Linked Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        currA = headA 
+        currB = headB
+        while currA or currB:
+
+            if currA == None:
+                currA = headB 
+            if currB == None:
+                currB = headA 
+            if currA == currB:
+                return currA 
+            currA = currA.next 
+            currB = currB.next
+        
+        return None
+
+# @lc code=end
+
